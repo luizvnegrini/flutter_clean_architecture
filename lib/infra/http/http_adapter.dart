5ac0ee390834279619422276c8c4e1357468e45a
Luiz Negrini <luizv.negrini@outlook.com>
2021-02-02 02:47:19 -0300

    move generic code to a private method

---
+++ lib/infra/http/http_adapter.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:meta/meta.dart';
+
+import '../../data/http/http.dart';
+
+class HttpAdapter implements HttpClient {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  @override
+  Future<Map> request({
+    @required String url,
+    @required String method,
+    Map body,
+  }) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+    };
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    final response = await client.post(url, headers: headers, body: jsonBody);
+
+    return _handleResponse(response);
+  }
+
+  Map _handleResponse(Response response) {
+    if (response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    } else {
+      return null;
+    }
+  }
+}
