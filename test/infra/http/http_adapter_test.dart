5d722ab380f25b98484e0d1edc1a11aee478af2c
Luiz Negrini <luizv.negrini@outlook.com>
2021-02-02 02:10:56 -0300

    ensure HttpAdapter returns null if post returns 200 with no data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -27,7 +27,7 @@ class HttpAdapter implements HttpClient {
     final jsonBody = body != null ? jsonEncode(body) : null;
     final response = await client.post(url, headers: headers, body: jsonBody);
 
-    return jsonDecode(response.body);
+    return response.body.isEmpty ? null : jsonDecode(response.body);
   }
 }
 
@@ -81,6 +81,16 @@ void main() {
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, {'any_key': 'any_value'});
+    });
+
+    test('should return null data if post returns 200 with no data', () async {
+      when(client.post(any, headers: anyNamed('headers'))).thenAnswer(
+        (_) async => Response('', 200),
+      );
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
     });
   });
 }
