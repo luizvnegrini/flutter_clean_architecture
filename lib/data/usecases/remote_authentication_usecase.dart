a05b551a360bbf79ce38d331f5cb783ced3b15b1
Luiz Negrini <luizv.negrini@outlook.com>
2021-02-02 01:31:00 -0300

    make remoteauthentication implements iauthentication

--- lib/data/usecases/remote_authentication_usecase.dart
+++ lib/data/usecases/remote_authentication_usecase.dart
@@ -8,12 +8,13 @@ import '../../domain/usecases/usecases.dart';
 import '../http/http.dart';
 import '../models/models.dart';
 
-class RemoteAuthentication {
+class RemoteAuthentication implements IAuthentication {
   final HttpClient httpClient;
   final String url;
 
   RemoteAuthentication({@required this.httpClient, @required this.url});
 
+  @override
   Future<AccountEntity> auth(AuthenticationParams params) async {
     final body = RemoteAuthenticationParams.fromDomain(params).toJson();
 
