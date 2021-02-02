7285d4cac62402a0fa2597ce250b137999c7f268
Luiz Negrini <luizv.negrini@outlook.com>
2021-02-02 01:52:39 -0300

    ensure HttpAdapter calls post with correct headers

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -16,7 +16,11 @@ class HttpAdapter {
     @required String method,
     Map body,
   }) async {
-    await client.post(url);
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+    };
+    await client.post(url, headers: headers);
   }
 }
 
@@ -29,7 +33,13 @@ void main() {
 
       await sut.request(url: url, method: 'post');
 
-      verify(client.post(url));
+      verify(client.post(
+        url,
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json',
+        },
+      ));
     });
   });
 }
