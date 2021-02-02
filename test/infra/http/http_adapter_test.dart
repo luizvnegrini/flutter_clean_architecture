a9973922bfa75ccfaac0dc9e5e4df37788d58aa1
Luiz Negrini <luizv.negrini@outlook.com>
2021-02-02 02:19:41 -0300

    move duplicated code to the setUp method

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -43,11 +43,17 @@ void main() {
   });
 
   group('post', () {
-    test('should call post with correct values', () async {
-      when(client.post(any, body: anyNamed('body'), headers: anyNamed('headers'))).thenAnswer(
-        (_) async => Response('{"any_key": "any_value"}', 200),
-      );
+    PostExpectation mockRequest() => when(client.post(any, body: anyNamed('body'), headers: anyNamed('headers')));
 
+    void mockResponse(int statusCode, {String body = '{"any_key": "any_value"}'}) {
+      mockRequest().thenAnswer((_) async => Response(body, statusCode));
+    }
+
+    setUp(() {
+      mockResponse(200);
+    });
+
+    test('should call post with correct values', () async {
       await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
       verify(client.post(
@@ -61,10 +67,6 @@ void main() {
     });
 
     test('should call post without body', () async {
-      when(client.post(any, headers: anyNamed('headers'))).thenAnswer(
-        (_) async => Response('{"any_key": "any_value"}', 200),
-      );
-
       await sut.request(url: url, method: 'post');
 
       verify(client.post(
@@ -74,19 +76,13 @@ void main() {
     });
 
     test('should return data if post returns 200', () async {
-      when(client.post(any, headers: anyNamed('headers'))).thenAnswer(
-        (_) async => Response('{"any_key": "any_value"}', 200),
-      );
-
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, {'any_key': 'any_value'});
     });
 
     test('should return null data if post returns 200 with no data', () async {
-      when(client.post(any, headers: anyNamed('headers'))).thenAnswer(
-        (_) async => Response('', 200),
-      );
+      mockResponse(200, body: '');
 
       final response = await sut.request(url: url, method: 'post');
 
