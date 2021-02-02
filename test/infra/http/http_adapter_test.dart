a195b371e5970f3869bb93ec94efd7fafe14cb56
Luiz Negrini <luizv.negrini@outlook.com>
2021-02-02 01:49:04 -0300

    ensure httpAdapter calls post with correct URL

---
+++ test/infra/http/http_adapter_test.dart
@@ -0,0 +1,35 @@
+import 'package:faker/faker.dart';
+import 'package:http/http.dart';
+import 'package:meta/meta.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+class ClientSpy extends Mock implements Client {}
+
+class HttpAdapter {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<void> request({
+    @required String url,
+    @required String method,
+    Map body,
+  }) async {
+    await client.post(url);
+  }
+}
+
+void main() {
+  group('post', () {
+    test('should call post with correct values', () async {
+      final client = ClientSpy();
+      final sut = HttpAdapter(client);
+      final url = faker.internet.httpUrl();
+
+      await sut.request(url: url, method: 'post');
+
+      verify(client.post(url));
+    });
+  });
+}
