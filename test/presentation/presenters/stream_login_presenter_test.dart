ba9e51d4c00346cb94fc904aaea1fd3de3a46df1
Luiz Negrini <luizv.negrini@outlook.com>
2021-02-05 20:27:37 -0300

    ensure EmailErrorStream will not emit duplicated event

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -35,8 +35,10 @@ void main() {
   test('should emit email error if validation fails', () {
     mockValidation(value: 'error');
 
-    expectLater(sut.emailErrorStream, emits('error'));
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, 'error')));
 
+    sut.validateEmail(email);
+    // ignore: cascade_invocations
     sut.validateEmail(email);
   });
 }
