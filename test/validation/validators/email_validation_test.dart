1c6dd93eef213df930b5433b9903c42bf007e185
Luiz Negrini <luizv.negrini@outlook.com>
2021-02-07 10:34:11 -0300

    move EmailValidation to its own file

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,21 +1,6 @@
-import 'package:home_automation/validation/protocols/ifield_validation.dart';
 import 'package:test/test.dart';
 
-class EmailValidation implements IFieldValidation {
-  @override
-  final String field;
-
-  EmailValidation(this.field);
-
-  @override
-  String validate(String value) {
-    final regex = RegExp(
-        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
-    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
-
-    return isValid ? null : 'Campo invávlido.';
-  }
-}
+import 'package:home_automation/validation/validators/validators.dart';
 
 void main() {
   EmailValidation sut;
