cb0dfa736967caa6b95d9bb78da64dc4d54883fb
Luiz Negrini <luizv.negrini@outlook.com>
2021-02-12 18:18:23 -0300

    ensure SplashScreenPage calls load current account on page load

--- test/ui/pages/splash_screen_page_test.dart
+++ test/ui/pages/splash_screen_page_test.dart
@@ -1,31 +1,69 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/foundation.dart';
+import 'package:meta/meta.dart';
 import 'package:get/route_manager.dart';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
 class SplashScreenPage extends StatelessWidget {
+  final ISplashScreenPresenter presenter;
+
+  const SplashScreenPage({@required this.presenter});
+
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: const Text('Home automation')),
-        body: const Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
+  Widget build(BuildContext context) {
+    presenter.loadCurrentAccount();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home automation')),
+      body: const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<ISplashScreenPresenter>('presenter', presenter));
+  }
 }
 
+abstract class ISplashScreenPresenter {
+  Future<void> loadCurrentAccount();
+}
+
+class SplashScreenPresenterSpy extends Mock implements ISplashScreenPresenter {}
+
 void main() {
-  Future<void> loadPage(WidgetTester tester) async => tester.pumpWidget(GetMaterialApp(
-        initialRoute: '/',
-        getPages: [
-          GetPage(
-            name: '/',
-            page: () => SplashScreenPage(),
-          )
-        ],
-      ));
+  SplashScreenPresenterSpy presenter;
+
+  Future<void> loadPage(WidgetTester tester) async {
+    presenter = SplashScreenPresenterSpy();
+
+    return tester.pumpWidget(GetMaterialApp(
+      initialRoute: '/',
+      getPages: [
+        GetPage(
+          name: '/',
+          page: () => SplashScreenPage(
+            presenter: presenter,
+          ),
+        )
+      ],
+    ));
+  }
 
   testWidgets('should present spinner on page load', (WidgetTester tester) async {
     await loadPage(tester);
 
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('should call LoadCurrentAccount on page load', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    verify(presenter.loadCurrentAccount()).called(1);
   });
 }
