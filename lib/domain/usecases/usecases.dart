b2ed067d3cb4ecff7caa4ca83c356b8f64154676
Luiz Negrini <luizv.negrini@outlook.com>
2021-02-16 11:24:20 -0300

    fix usecases.dart

--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -1,6 +1,6 @@
-export './add_account.dart';
 export './add_account_prams.dart';
 export './authentication_params.dart';
+export './iadd_account.dart';
 export './iauthentication.dart';
 export './iload_current_account.dart';
 export './isave_current_account.dart';
