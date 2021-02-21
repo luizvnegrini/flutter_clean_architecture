db8503e7f60ae4402af4d3b3fbe7afb08504fad8
Luiz Negrini <luizv.negrini@outlook.com>
2021-02-21 20:58:59 -0300

    test: sneure HttpAdapter throws BadRequest on 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -204,5 +204,21 @@ void main() {
 
       expect(response, null);
     });
+
+    test('should return BadRequestError if get returns 400', () async {
+      mockResponse(400);
+
+      final future = sut.request(url: url, method: 'get');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('should return BadRequestError if get returns 400', () async {
+      mockResponse(400, body: '');
+
+      final future = sut.request(url: url, method: 'get');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
   });
 }
