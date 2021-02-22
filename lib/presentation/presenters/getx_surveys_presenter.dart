9071e2468d11923eea76f14e7d30a2cdf396923f
Luiz Negrini <luizv.negrini@outlook.com>
2021-02-22 17:04:18 -0300

    test: move GetxSurveysPresenter to production file

---
+++ lib/presentation/presenters/getx_surveys_presenter.dart
@@ -0,0 +1,40 @@
+import 'package:get/get.dart';
+import 'package:home_automation/domain/enums/enums.dart';
+import 'package:home_automation/ui/helpers/errors/errors.dart';
+import 'package:intl/intl.dart';
+import 'package:meta/meta.dart';
+
+import '../../domain/usecases/usecases.dart';
+import '../../ui/pages/surveys/surveys.dart';
+import '../../utils/extensions/enum_extensions.dart';
+
+class GetxSurveysPresenter {
+  final ILoadSurveys loadSurveys;
+
+  final _isLoadingObserver = true.obs;
+  final _surveys = Rx<List<SurveyViewModel>>();
+
+  Stream<bool> get isLoadingStream => _isLoadingObserver.stream;
+  Stream<List<SurveyViewModel>> get surveysStream => _surveys.stream;
+
+  GetxSurveysPresenter({@required this.loadSurveys});
+
+  Future<void> loadData() async {
+    try {
+      _isLoadingObserver.value = true;
+      final surveys = await loadSurveys.load();
+      _surveys.value = surveys
+          .map((survey) => SurveyViewModel(
+                id: survey.id,
+                question: survey.question,
+                date: DateFormat('dd MMM yyyy').format(survey.dateTime),
+                didAnswer: survey.didAnswer,
+              ))
+          .toList();
+    } on DomainError {
+      _surveys.subject.addError(UIError.unexpected.description);
+    } finally {
+      _isLoadingObserver.value = false;
+    }
+  }
+}
--- test/presentation/presenters/getx_surveys_presenter_test.dart
+++ test/presentation/presenters/getx_surveys_presenter_test.dart
@@ -1,47 +1,14 @@
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
-import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 
+import 'package:home_automation/presentation/presenters/presenters.dart';
 import 'package:home_automation/domain/enums/enums.dart';
 import 'package:home_automation/ui/helpers/errors/errors.dart';
 import 'package:home_automation/ui/pages/surveys/surveys.dart';
 import 'package:home_automation/domain/entities/survey_entity.dart';
 import 'package:home_automation/domain/usecases/usecases.dart';
 import 'package:home_automation/utils/extensions/enum_extensions.dart';
-
-class GetxSurveysPresenter {
-  final ILoadSurveys loadSurveys;
-
-  final _isLoadingObserver = true.obs;
-  final _surveys = Rx<List<SurveyViewModel>>();
-
-  Stream<bool> get isLoadingStream => _isLoadingObserver.stream;
-  Stream<List<SurveyViewModel>> get surveysStream => _surveys.stream;
-
-  GetxSurveysPresenter({@required this.loadSurveys});
-
-  Future<void> loadData() async {
-    try {
-      _isLoadingObserver.value = true;
-      final surveys = await loadSurveys.load();
-      _surveys.value = surveys
-          .map((survey) => SurveyViewModel(
-                id: survey.id,
-                question: survey.question,
-                date: DateFormat('dd MMM yyyy').format(survey.dateTime),
-                didAnswer: survey.didAnswer,
-              ))
-          .toList();
-    } on DomainError {
-      _surveys.subject.addError(UIError.unexpected.description);
-    } finally {
-      _isLoadingObserver.value = false;
-    }
-  }
-}
 
 class LoadSurveysSpy extends Mock implements ILoadSurveys {}
 
