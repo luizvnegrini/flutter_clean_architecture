d62d405435723cf46896d1dee8fef6def0410e84
Luiz Negrini <luizv.negrini@outlook.com>
2021-02-22 15:31:47 -0300

    test: ensure SurveysPage present correct data on list

--- lib/ui/pages/surveys/components/survey_item.dart
+++ lib/ui/pages/surveys/components/survey_item.dart
@@ -14,7 +14,7 @@ class SurveyItem extends StatelessWidget {
         child: Container(
           padding: const EdgeInsets.all(20),
           decoration: BoxDecoration(
-            color: Theme.of(context).secondaryHeaderColor,
+            color: viewModel.didAnswer ? Theme.of(context).secondaryHeaderColor : Theme.of(context).primaryColorDark,
             boxShadow: const [
               BoxShadow(
                 offset: Offset(0, 1),
@@ -28,9 +28,9 @@ class SurveyItem extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text(
-                '22 Fev 2021',
-                style: TextStyle(
+              Text(
+                viewModel.date,
+                style: const TextStyle(
                   color: Colors.white,
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
--- test/ui/pages/surveys_page_test.dart
+++ test/ui/pages/surveys_page_test.dart
@@ -41,8 +41,8 @@ void main() {
   }
 
   List<SurveyViewModel> makeSurveys() => [
-        SurveyViewModel(id: '1', question: 'Question 1', date: 'any date', didAnswer: true),
-        SurveyViewModel(id: '2', question: 'Question 2', date: 'any date', didAnswer: false),
+        SurveyViewModel(id: '1', question: 'Question 1', date: 'Date 1', didAnswer: true),
+        SurveyViewModel(id: '2', question: 'Question 2', date: 'Date 2', didAnswer: false),
       ];
 
   setUp(() {
@@ -103,5 +103,7 @@ void main() {
     expect(find.text('Recarregar'), findsNothing);
     expect(find.text('Question 1'), findsWidgets);
     expect(find.text('Question 2'), findsWidgets);
+    expect(find.text('Date 1'), findsWidgets);
+    expect(find.text('Date 2'), findsWidgets);
   });
 }
