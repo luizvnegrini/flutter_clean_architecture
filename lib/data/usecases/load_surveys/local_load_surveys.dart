261bdb9835e6a38b6e0e1a7593ab2b7fc70b7885
Luiz Negrini <luizv.negrini@outlook.com>
2021-02-24 15:56:46 -0300

    refactor: make LocalLoadSurveys implements ILoadSurveys

--- lib/data/usecases/load_surveys/local_load_surveys.dart
+++ lib/data/usecases/load_surveys/local_load_surveys.dart
@@ -4,12 +4,14 @@ import '../../../data/cache/cache.dart';
 import '../../../data/models/models.dart';
 import '../../../domain/entities/entities.dart';
 import '../../../domain/enums/enums.dart';
+import '../../../domain/usecases/usecases.dart';
 
-class LocalLoadSurveys {
+class LocalLoadSurveys implements ILoadSurveys {
   final IFetchCacheStorage fetchCacheStorage;
 
   LocalLoadSurveys({@required this.fetchCacheStorage});
 
+  @override
   Future<List<SurveyEntity>> load() async {
     try {
       final data = await fetchCacheStorage.fetch('surveys');
