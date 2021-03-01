cdde33c41ac4fe48043dc220849d368f85db94d3
Luiz Negrini <luizv.negrini@outlook.com>
2021-03-01 18:49:16 -0300

    test: ensure Composite calls local save with remote data

--- test/main/composites/remote_load_survey_result_with_local_fallback_test.dart
+++ test/main/composites/remote_load_survey_result_with_local_fallback_test.dart
@@ -1,4 +1,5 @@
 import 'package:faker/faker.dart';
+import 'package:home_automation/domain/entities/entities.dart';
 import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
@@ -7,26 +8,59 @@ import 'package:home_automation/data/usecases/usecases.dart';
 
 class RemoteLoadSurveyResultWithLocalFallback {
   final RemoteLoadSurveyResult remote;
+  final LocalLoadSurveyResult local;
 
-  RemoteLoadSurveyResultWithLocalFallback({@required this.remote});
+  RemoteLoadSurveyResultWithLocalFallback({@required this.remote, @required this.local});
 
   Future<void> loadBySurvey({String surveyId}) async {
-    await remote.loadBySurvey(surveyId: surveyId);
+    final surveyResult = await remote.loadBySurvey(surveyId: surveyId);
+    await local.save(surveyId: surveyId, surveyResult: surveyResult);
   }
 }
 
 class RemoteLoadSurveyResultSpy extends Mock implements RemoteLoadSurveyResult {}
 
-void main() {
-  test('should call remote LoadBySurvey', () async {
-    final surveyId = faker.guid.guid();
-    final remote = RemoteLoadSurveyResultSpy();
-    final sut = RemoteLoadSurveyResultWithLocalFallback(
-      remote: remote,
-    );
+class LocalLoadSurveyResultSpy extends Mock implements LocalLoadSurveyResult {}
 
+void main() {
+  RemoteLoadSurveyResultWithLocalFallback sut;
+  RemoteLoadSurveyResultSpy remote;
+  LocalLoadSurveyResultSpy local;
+  String surveyId;
+  SurveyResultEntity surveyResult;
+
+  void mockSurveyResult() {
+    surveyResult = SurveyResultEntity(surveyId: faker.guid.guid(), question: faker.lorem.sentence(), answers: [
+      SurveyAnswerEntity(
+        answer: faker.lorem.sentence(),
+        isCurrentAnswer: faker.randomGenerator.boolean(),
+        percent: faker.randomGenerator.integer(100),
+      ),
+    ]);
+    when(remote.loadBySurvey(surveyId: anyNamed('surveyId'))).thenAnswer((_) async => surveyResult);
+  }
+
+  setUp(() {
+    remote = RemoteLoadSurveyResultSpy();
+    local = LocalLoadSurveyResultSpy();
+    sut = RemoteLoadSurveyResultWithLocalFallback(
+      remote: remote,
+      local: local,
+    );
+    surveyId = faker.guid.guid();
+
+    mockSurveyResult();
+  });
+
+  test('should call remote LoadBySurvey', () async {
     await sut.loadBySurvey(surveyId: surveyId);
 
     verify(remote.loadBySurvey(surveyId: surveyId)).called(1);
+  });
+
+  test('should call local save with remote data', () async {
+    await sut.loadBySurvey(surveyId: surveyId);
+
+    verify(local.save(surveyId: surveyId, surveyResult: surveyResult)).called(1);
   });
 }
